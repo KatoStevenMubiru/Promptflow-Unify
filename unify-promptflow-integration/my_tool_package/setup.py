0d65843ccc7de93336915d1a4b358a4b897cd6d1
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-07-31 08:52:39 +0300

    Create setup.py

---
+++ unify-promptflow-integration/my_tool_package/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "my_tool_package"
+
+setup(
+    name=PACKAGE_NAME,
+    version="0.0.1",
+    description="This is my tools package",
+    packages=find_packages(),
+    entry_points={
+        "package_tools": [
+            "my_tool = my_tool_package.tools.utils:list_package_tools"
+        ],
+    },
+    include_package_data=True,  # This line tells setuptools to include files from MANIFEST.in  # noqa: E501
+)
